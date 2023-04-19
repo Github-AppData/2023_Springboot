e92c0bd7c724b5e070717ee82ba5ef67abde9555
Github-AppData <77886661+Github-AppData@users.noreply.github.com>
2023-04-19 16:46:14 +0900

    7주차

---
+++ myself-project/src/main/java/com/rubypaper/board/MyselfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rubypaper.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyselfProjectApplication.class, args);
+	}
+
+}
---
+++ myself-spring-boot-starter/src/main/java/com/rubypp/board/myself_spring_boot_starter/App.java
@@ -0,0 +1,13 @@
+package com.rubypp.board.myself_spring_boot_starter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
